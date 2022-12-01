a84c7d823fbd190a80670e0dc2f8a7e302efb01d
robin <robenjay@hotmail.nl>
2022-12-01 11:03:12 +0100

    style indenting

--- mobile/styles/BasicStyles.js
+++ mobile/styles/BasicStyles.js
@@ -1,39 +1,39 @@
 import { StyleSheet } from "react-native";
 
 export const styles = StyleSheet.create({
-  layout: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-    backgroundColor: "#FFF5D2",
-    margin: 0,
-    padding: 0,
-  },
-  innerLayout: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-    backgroundColor: "#FFEFB9",
-    margin: 20,
-    padding: 0,
-    width: "95%",
-  },
-  headerPage: {
-    flex: 1,
-    alignItems: "center",
-    margin: 10,
-    padding: 0,
-    width: "95%",
-  },
-  title: {
-    fontSize: 30,
-    paddingTop: 20,
-    fontWeight: "600",
-  },
-  inlineIconText: {
-    paddingVertical: 0,
-    paddingHorizontal: 0,
-    flexDirection: "row",
-    alignItems: "center",
-  },
+    layout: {
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+        backgroundColor: "#FFF5D2",
+        margin: 0,
+        padding: 0,
+    },
+    innerLayout: {
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+        backgroundColor: "#FFEFB9",
+        margin: 20,
+        padding: 0,
+        width: "95%",
+    },
+    headerPage: {
+        flex: 1,
+        alignItems: "center",
+        margin: 10,
+        padding: 0,
+        width: "95%",
+    },
+    title: {
+        fontSize: 30,
+        paddingTop: 20,
+        fontWeight: "600",
+    },
+    inlineIconText: {
+        paddingVertical: 0,
+        paddingHorizontal: 0,
+        flexDirection: "row",
+        alignItems: "center",
+    },
 });
