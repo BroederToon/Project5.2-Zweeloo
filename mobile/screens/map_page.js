e16bc631762e99b56388a7f0c1c6f88ae161d17d
Yannieck Blaauw <yannieck.blaauw@student.nhlstenden.com>
2022-12-02 15:06:01 +0100

    Added markers and nodes to the map and made them clickable

--- mobile/screens/map_page.js
+++ mobile/screens/map_page.js
@@ -4,6 +4,7 @@ import { styles } from "../styles/basic_styles";
 import MapView, { Geojson, Marker } from "react-native-maps";
 import { map } from "../styles/map_page_styles";
 import { useEffect, useState } from "react";
+import { StatusBar } from "expo-status-bar";
 
 const Map_page = () => {
     const nav = useNavigation();
@@ -12,22 +13,48 @@ const Map_page = () => {
 
     useEffect(() => {
         getRoute();
-        console.log(data.poi);
-        console.log(data.poi.length);
     }, []);
 
     const getRoute = async () => {
-        fetch("http://10.232.14.4:3000/api/routes/route/1")
+        await fetch("http://192.168.95.220:3000/api/routes/route/1")
             .then((response) => response.json())
             .then((json) => setData(json))
             .catch((error) => console.error(error))
             .finally(() => setLoading(false));
     };
 
-    const getPOI = () => {
+    const onMarkerClick = (id) => {
+        console.log(id);
+    }
+
+    const getMarkers = () => {
+        let markers = [];
+        //POIs
         for (let i = 0; i < data.poi.length; i++) {
-            return <Marker key={data.poi[i].id} />;
+            markers.push(
+                <Marker
+                    key={data.poi[i].id}
+                    coordinate={{
+                        latitude: parseFloat(data.poi[i].lat),
+                        longitude: parseFloat(data.poi[i].lon),
+                    }}
+                    onPress = {() => onMarkerClick(data.poi[i].id)}
+                />
+            );
         }
+        //Nodes
+        for (let i = 0; i < data.node.length; i++) {
+            markers.push(
+                <Marker
+                    key={data.node[i].id}
+                    coordinate={{
+                        latitude: parseFloat(data.poi[i].lat),
+                        longitude: parseFloat(data.poi[i].lon),
+                    }}
+                />
+            );
+        }
+        return markers;
     };
 
     return (
@@ -36,22 +63,27 @@ const Map_page = () => {
                 <View style={styles.headerPage}>
                     <Text style={styles.title}>{data.name}</Text>
                 </View>
-                <MapView
-                    style={map.mapView}
-                    initialRegion={{
-                        latitude: 52.784155118378585,
-                        longitude: 6.907284749200984,
-                        latitudeDelta: 0.1,
-                        longitudeDelta: 0.1,
-                    }}
-                >
-                    <Geojson
-                        geojson={data.route}
-                        strokeColor="red"
-                        fillColor="green"
-                        strokeWidth={4}
-                    />
-                </MapView>
+                {isLoading ? (
+                    <StatusBar />
+                ) : (
+                    <MapView
+                        style={map.mapView}
+                        initialRegion={{
+                            latitude: 52.794703304265546,
+                            longitude: 6.72937774862828,
+                            latitudeDelta: 0.01,
+                            longitudeDelta: 0.01,
+                        }}
+                    >
+                        <Geojson
+                            geojson={data.route}
+                            strokeColor="red"
+                            fillColor="green"
+                            strokeWidth={4}
+                        />
+                        {getMarkers()}
+                    </MapView>
+                )}
             </View>
         </View>
     );
