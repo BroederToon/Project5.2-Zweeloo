68660e4fc7a35259d599b2948925653e9b45d9dc
FrancaBaars <franca.baars@student.nhlstenden.com>
2022-12-08 09:37:40 +0100

    added Icons to markers

--- mobile/screens/map_page.js
+++ mobile/screens/map_page.js
@@ -1,4 +1,4 @@
-import { Text, View } from "react-native";
+import { Text, View, Image } from "react-native";
 import { useNavigation } from "@react-navigation/native";
 import { styles } from "../styles/basic_styles";
 import MapView, { Geojson, Marker } from "react-native-maps";
@@ -10,13 +10,14 @@ const Map_page = () => {
     const nav = useNavigation();
     const [data, setData] = useState([]);
     const [isLoading, setLoading] = useState(true);
+    const [string, setString] = useState(null);
 
     useEffect(() => {
         getRoute();
     }, []);
 
     const getRoute = async () => {
-        await fetch("http://192.168.95.220:3000/api/routes/route/1")
+        await fetch("http://10.232.0.243:3000/api/routes/route/1")
             .then((response) => response.json())
             .then((json) => setData(json))
             .catch((error) => console.error(error))
@@ -25,34 +26,68 @@ const Map_page = () => {
 
     const onMarkerClick = (id) => {
         console.log(id);
-    }
+    };
+
+    const getImage = (type) => {
+        if (type == "INFO") {
+            setString("../assets/info.png");
+            return;
+        } else if (type == "POI") {
+            setString("../assets/poi.png");
+            return;
+        } else if (type == "INVIS") {
+            setString("nisk");
+            return;
+        } else if (type == "CAFE") {
+            setString("../assets/Cafe.png");
+            return;
+        } else {
+            setString("niks");
+            return;
+        }
+    };
 
     const getMarkers = () => {
         let markers = [];
+        let index = 0;
+        console.log(data.poi.length);
         //POIs
         for (let i = 0; i < data.poi.length; i++) {
+            getImage(data.poi[i].type);
+            console.log(data.poi[i].type);
             markers.push(
                 <Marker
-                    key={data.poi[i].id}
+                    key={index}
                     coordinate={{
                         latitude: parseFloat(data.poi[i].lat),
                         longitude: parseFloat(data.poi[i].lon),
                     }}
-                    onPress = {() => onMarkerClick(data.poi[i].id)}
-                />
+                    onPress={() => onMarkerClick(data.poi[i].id)}
+                >
+                    <Image
+                        style={map.logo}
+                        source={require("../assets/Cafe.png")}
+                    />
+                </Marker>
             );
+            index++;
         }
         //Nodes
         for (let i = 0; i < data.node.length; i++) {
             markers.push(
                 <Marker
-                    key={data.node[i].id}
+                    key={index}
                     coordinate={{
-                        latitude: parseFloat(data.poi[i].lat),
-                        longitude: parseFloat(data.poi[i].lon),
+                        latitude: parseFloat(data.node[i].lat),
+                        longitude: parseFloat(data.node[i].lon),
                     }}
-                />
+                >
+                    <View style={map.node}>
+                        <Text style={map.nodeText}>{data.node[i].index}</Text>
+                    </View>
+                </Marker>
             );
+            index++;
         }
         return markers;
     };
--- mobile/styles/map_page_styles.js
+++ mobile/styles/map_page_styles.js
@@ -5,4 +5,18 @@ export const map = StyleSheet.create({
         width: "100%",
         height: "90%",
     },
+    node: {
+        backgroundColor: "black",
+        borderRadius: 10,
+    },
+    nodeText: {
+        color: "white",
+        padding: 7,
+        fontWeight: "bold",
+        fontSize: 15,
+    },
+    logo: {
+        width: 50,
+        height: 50,
+    },
 });
