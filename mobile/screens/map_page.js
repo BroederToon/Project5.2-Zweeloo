3055ef70c2c9ba223d32adafe4b0fc785301d4c9
FrancaBaars <franca.baars@student.nhlstenden.com>
2022-12-13 13:40:40 +0100

    removed unused imports

--- mobile/screens/map_page.js
+++ mobile/screens/map_page.js
@@ -1,10 +1,6 @@
-import { Text, View, Image } from "react-native";
+import { View } from "react-native";
 import { useNavigation } from "@react-navigation/native";
 import { styles } from "../styles/basic_styles";
-import MapView, { Geojson, Marker } from "react-native-maps";
-import { map } from "../styles/map_page_styles";
-import { useCallback, useEffect, useState } from "react";
-import { StatusBar } from "expo-status-bar";
 import getMapPage from "./map_page_function";
 
 const Map_page = () => {
