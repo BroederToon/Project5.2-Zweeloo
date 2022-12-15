4d00a84195a846905974f7ec384c0efa1bf494b8
FrancaBaars <franca.baars@student.nhlstenden.com>
2022-12-15 14:36:34 +0100

    changed gitignore and added env file

--- mobile/screens/Home.js
+++ mobile/screens/Home.js
@@ -1,9 +1,9 @@
 import { styles } from "../styles/basic_styles";
 import { Text, View } from "react-native";
-import getLocationPermission from "./../function/location_permission";
+import GetLocationPermission from "../function/location_permission";
 
 const Home = () => {
-    getLocationPermission();
+    GetLocationPermission();
     return (
         <View style={styles.layout}>
             <Text style={styles.title}>starte</Text>
